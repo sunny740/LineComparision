373f867f43868ab388d5713fe195315e408aec89
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-08 23:19:42 +0530

    [Sunny] Add: WelcomeToLineComparision

---
+++ LineComparisionComputation/LineComparisionProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionComputation
+{
+    public class LineComparisionProgram
+    {
+        public void Welcome()
+        {
+            Console.WriteLine("This is a program for Line Comparision");
+            Console.WriteLine();
+        }
+    }
+}
